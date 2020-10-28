82a416f4d6b7ad0ddfec171613c5b213e552d9ba
Luis Felipe R <luisfelroca@hotmail.com>
2020-10-27 19:08:18 -0500

    inicio de code phone

---
+++ src/app/models/Person.ts
@@ -0,0 +1,11 @@
+export class Person {
+  constructor(
+    public id: string,
+    public name: string,
+    public lastname: string,
+    public password: string,
+    public email: string,
+    public phone: string,
+    public urlPicProfile: string
+  ) {}
+}
